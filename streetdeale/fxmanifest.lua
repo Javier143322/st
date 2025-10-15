b67a1766d92ef3703ae0bc6196a222080c57ce33
Javier143322 <huhdnuhdl@gmail.com>
2025-10-14 23:43:04 -0500

    Actualizar fxmanifest.lua

--- streetdeale/fxmanifest.lua
+++ streetdeale/fxmanifest.lua
@@ -1,1 +1,24 @@
+-- fxmanifest.lua
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+-- DEPENDENCIA CLAVE: ESX
+dependencies {
+    'es_extended'
+    -- Puedes añadir aquí la dependencia del menú o target que uses (esx_menu_default, qtarget, etc.)
+}
+
+shared_scripts {
+    '@es_extended/locale.lua', -- Usar locale de ESX
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
 
